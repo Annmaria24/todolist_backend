720f77933326c6d5710c99fd1179d1aa33217d41
Annmaria24 <annmariak24@gmail.com>
2025-04-02 10:37:47 +0530

    next commit

--- Routes/Listroutes.js
+++ Routes/Listroutes.js
@@ -23,4 +23,33 @@ router.get("/", async (req, res) => {
         res.status(400).json({ error: error.message });
     }
 });
+
+router.put('/:id', async (req, res) => {
+    try {
+        const { id } = req.params;
+        const updatedItem = await Item.findByIdAndUpdate(id, req.body, { new: true });
+        if (!updatedItem) {
+            return res.status(404).json({ message: 'Item not found' });
+        }
+        res.status(200).json(updatedItem);
+    } catch (error) {
+        res.status(400).json({ error: error.message });
+    }
+});
+
+// Delete an existing item
+router.delete('/:id', async (req, res) => {
+    try {
+        const { id } = req.params;
+        const deletedItem = await Item.findByIdAndDelete(id);
+        if (!deletedItem) {
+            return res.status(404).json({ message: 'Item not found' });
+        }
+        res.status(200).json({ message: 'Item deleted successfully' });
+    } catch (error) {
+        res.status(400).json({ error: error.message });
+    }
+});
+
+
 module.exports = router;
